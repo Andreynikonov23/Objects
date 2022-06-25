474302759f742da5896ca52675efe5c51ff4b956
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-06-25 14:34:35 +0300

    Delete Inner directory

--- Inner/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SuperUser.User user = new SuperUser().new User("George", "New Zeland");
-        SuperUser superUser = new SuperUser(user);
-        System.out.println(superUser.getDescription());
-    }
-}
